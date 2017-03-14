7d27f8c852ae8e07a61d3a9a799bc88dd438a95e
songtianyi <tianyi.song@ucloud.cn>
2017-03-14 13:56:51 +0800

    fix cannot assign request addresss

--- api-tester.go
+++ api-tester.go
@@ -8,6 +8,8 @@ import (
 	"github.com/songtianyi/rrframework/utils"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"sync/atomic"
 	"time"
 )
 
@@ -18,7 +20,32 @@ var (
 	_ = flag.Int("c", 1, "Number of multiple requests to make at a time")
 	_ = flag.Int("n", 1, "Number of requests to perform")
 	_ = flag.String("T", "image/jpeg", "Content-Type")
+	_ = flag.Bool("strict", false, "Strict mode, if it's true tester will exit when any request fail")
 )
+
+// stat vars
+var (
+	totalCost = uint64(0)
+	maxCost   = uint64(0)
+	minCost   = uint64(1<<32 - 1)
+	success   = uint64(0)
+	failed    = uint64(0)
+)
+
+func printStat() {
+	// print stat
+	tick := time.Tick(2 * time.Second)
+	for {
+		select {
+		case <-tick:
+			if success < 1 {
+				break
+			}
+			logs.Info("QPS", success*1000000/totalCost, "TotalCost", totalCost, "AvgCost", totalCost/success, "MaxCost", maxCost, "MinCost", minCost,
+				"Success", success, "Failed", failed)
+		}
+	}
+}
 
 func main() {
 
@@ -50,9 +77,18 @@ func main() {
 	med, _ := rrutils.FlagGetString("method")
 	uri, _ := rrutils.FlagGetString("uri")
 	cot, _ := rrutils.FlagGetString("T")
+	strict, _ := rrutils.FlagGetBool("strict")
 
 	// common http client
-	client := &http.Client{}
+	client := &http.Client{
+		Transport: &http.Transport{
+			DisableKeepAlives:   false,
+			MaxIdleConnsPerHost: 40,
+		},
+	}
+
+	// stat
+	go printStat()
 
 	// dispatch
 	tick := time.Tick(2 * time.Second)
@@ -75,11 +111,24 @@ loop1:
 				err := do(client, req, sema)
 				end := time.Now().UnixNano()
 
-				// stat and output
-				logs.Debug("cost", (end-start)/1000000)
+				// stat
 				if err != nil {
+					atomic.AddUint64(&failed, 1)
 					logs.Error(err)
+					if strict {
+						os.Exit(0)
+					}
+					return
 				}
+				atomic.AddUint64(&success, 1)
+				cost := uint64((end - start) / 1000000)
+				if cost < minCost {
+					minCost = cost
+				}
+				if cost > maxCost {
+					maxCost = cost
+				}
+				atomic.AddUint64(&totalCost, cost)
 			}(sema)
 
 		case <-tick:
@@ -99,9 +148,9 @@ func do(client *http.Client, req *http.Request, sema chan struct{}) error {
 	if err != nil {
 		return err
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
 		body, _ := ioutil.ReadAll(resp.Body)
-		defer resp.Body.Close()
 		return fmt.Errorf("StatusCode %d, Body %s", resp.StatusCode, string(body))
 	}
 	return nil
